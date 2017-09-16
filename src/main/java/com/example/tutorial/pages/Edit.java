a800cf1cd5b9fbaf17552ea378220216ba711b2a
BrandonRudiselNDSU <brandonrudisel@gmail.com>
2017-09-15 19:12:41 -0500

    Grids display data
    Grids in Edit and Delete now display data

---
+++ src/main/java/com/example/tutorial/pages/Edit.java
@@ -0,0 +1,17 @@
+package com.example.tutorial.pages;
+
+import java.util.List;
+
+import org.apache.tapestry5.ioc.annotations.Inject;
+import org.hibernate.Session;
+
+import com.example.tutorial.entities.Address;
+
+public class Edit {
+	@Inject
+	private Session session;
+	
+	public List<Address> getAddresses() {
+		return session.createCriteria(Address.class).list();
+	}
+}
